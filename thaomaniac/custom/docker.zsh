dcc969b8989f89a661b7fc5f638f51c797ef6832
Vu Thanh Thao <thaomaniac@gmail.com>
2024-08-02 09:37:40 +0700

    Update docker map php

--- thaomaniac/custom/docker.zsh
+++ thaomaniac/custom/docker.zsh
@@ -89,7 +89,7 @@ _m2DockerPhpVerFile() {
   local phpVer='php'
 
   if [ -f "$file" ]; then
-    while IFS=: read -r folder php_version; do
+    while IFS="=" read -r folder php_version; do
       if [[ "$PWD" == *"/$folder" ]]; then
         phpVer=$php_version
         break
