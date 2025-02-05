95c45e5f3aa526c08480f468d606c2c37bb7447b
Vu Thanh Thao <thaomaniac@gmail.com>
2025-02-05 17:36:34 +0700

    Add new plugins & function customload

--- thaomaniac/custom/initialize.zsh
+++ thaomaniac/custom/initialize.zsh
@@ -10,39 +10,59 @@ if [[ -s "$ZTMDIR/init.zsh" ]]; then
 fi
 
 # Customize to your needs...
+function tmcustomload {
+  local file
 
-#!/usr/bin/env zsh
-# load custom command file
-typeset -a TM_CUSTOM_FILE=(
-  agnoster # Override Agnoster theme
-  alias # Alias & custom function
-  git # Git
-  magento # Magento CLI
-  docker # Docker CLI
-  terminal # Custom terminal function
-)
+  # load custom command files
+  local -a TM_CUSTOM_FILE=(
+    agnoster # Override Agnoster theme
+    alias    # Alias & custom function
+    git      # Git
+    magento  # Magento CLI
+    docker   # Docker CLI
+    terminal # Custom terminal function
+  )
 
-for file in "${TM_CUSTOM_FILE[@]}"; do
-  file=$ZTMDIR/custom/$file.zsh
-  if [[ -f $file ]]; then
-    source $file
-  fi
-done
+  for file in "${TM_CUSTOM_FILE[@]}"; do
+    file=$ZTMDIR/custom/$file.zsh
+    if [[ -f $file ]]; then
+      source "$file"
+    fi
+  done
 
-# load completions
-fpath=($ZTMDIR/completions $fpath)
+  # load completions
+  fpath=($ZTMDIR/completions $fpath)
 
-# load local file (not in git)
-for file in $ZTMDIR/local/*; do
-  if [[ -f $file ]]; then
-    source $file
-  fi
-done
+  # load local file (not in git)
+  for file in $ZTMDIR/local/*; do
+    if [[ -f $file ]]; then
+      source "$file"
+    fi
+  done
 
-#----- load bg notify -----#
-if [[ -s "$ZTMDIR/plugins/bgnotify/bgnotify.plugin.zsh" ]]; then
-  source "$ZTMDIR/plugins/bgnotify/bgnotify.plugin.zsh"
-fi
+  # load external plugins
+  local plugin
+  local plugin_dir
+  local TM_CUSTOM_PLUGINS
+
+  zstyle ':tm:custom:plugins' plugins \
+    bgnotify \
+    you-should-use
+
+  zstyle -g TM_CUSTOM_PLUGINS ':tm:custom:plugins' plugins
+  for plugin in "${TM_CUSTOM_PLUGINS[@]}"; do
+    plugin_dir=${ZTMDIR}/plugins/${plugin}
+    if [[ ! -d "$plugin_dir" ]]; then
+      echo "$0: Missing custom plugin dir: $plugin_dir"
+    fi
+    if [[ -s "${plugin_dir}/init.zsh" ]]; then
+      source "${plugin_dir}/init.zsh"
+    elif [[ -s "${plugin_dir}/${plugin}.plugin.zsh" ]]; then
+      source "${plugin_dir}/${plugin}.plugin.zsh"
+    fi
+  done
+}
+tmcustomload
 
 # remove background color 'ls'
 export LS_COLORS="$LS_COLORS:ow=1;34:tw=1;34:"
